b15ba6ccbb94c469714a2db81ec88ca6a7045213
koji.sato <koji_sato@cybozu.co.jp>
2023-08-05 13:43:33 +0900

    implement getting started after webpack

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require("path");
+const outputpath = path.resolve(__dirname, "dist");
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: outputpath,
+  },
+};
