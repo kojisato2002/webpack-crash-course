463abc18dbf612382816aec0499e9db963625c20
koji.sato <koji_sato@cybozu.co.jp>
2023-08-07 11:24:45 +0900

    install webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require("path");
 const outputpath = path.resolve(__dirname, "dist");
+
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: outputpath,
+  },
+  devServer: {
+    contentBase: outputpath,
   },
 };
